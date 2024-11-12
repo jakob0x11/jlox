92715cc04fcb28f5354dd9424c73bb72f1f8088b
Jakob <horvath.j17@gmail.com>
2024-11-11 19:17:45 -0700

    begin work on functions

--- lox/src/com/craftinginterpreters/tool/GenerateAst.java
+++ lox/src/com/craftinginterpreters/tool/GenerateAst.java
@@ -13,8 +13,9 @@ public class GenerateAst {
         }
         String outputDir = args[0];
         defineAst(outputDir, "Expr", Arrays.asList(
-            "Assign  : Token name, Expr value",
+            "Assign   : Token name, Expr value",
             "Binary   : Expr left, Token operator, Expr right",
+            "Call     : Expr callee, Token paren, List<Expr> arguments",
             "Grouping : Expr expression",
             "Literal  : Object value",
             "Logical  : Expr left, Token operator, Expr right",
