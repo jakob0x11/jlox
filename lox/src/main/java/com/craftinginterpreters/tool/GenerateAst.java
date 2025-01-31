86d966e996b97c07e41e7b64dac13fca126600fc
Jakob <horvath.j17@gmail.com>
2025-01-30 20:20:48 -0700

    convert to maven project, begin work on functions

--- lox/src/main/java/com/craftinginterpreters/tool/GenerateAst.java
+++ lox/src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -13,8 +13,9 @@ public class GenerateAst {
         }
         String outputDir = args[0];
         defineAst(outputDir, "Expr", Arrays.asList(
-            "Assign  : Token name, Expr value",
+            "Assign   : Token name, Expr value",
             "Binary   : Expr left, Token operator, Expr right",
+            "Call     : Expr callee, Token paren, List<Expr> arguments",
             "Grouping : Expr expression",
             "Literal  : Object value",
             "Logical  : Expr left, Token operator, Expr right",
@@ -25,9 +26,10 @@ public class GenerateAst {
         defineAst(outputDir, "Stmt", Arrays.asList(
             "Block : List<Stmt> statements",
             "Expression      : Expr expression",
-            "If              : Expr condition, Stmt thenBranch," +
-                             " Stmt elseBranch",
+            "Function        : Token name, List<Token> params, List<Stmt> body",
+            "If              : Expr condition, Stmt thenBranch, Stmt elseBranch",
             "Print           : Expr expression",
+            "Return          : Token keyword, Expr value",
             "Var             : Token name, Expr initializer",
             "While           : Expr condition, Stmt body"
         ));
